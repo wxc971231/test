162522befa9542671d3f24d2ca9f18dd8db6825f
wxc971231 <wwxc971231@163.com>
2019-08-27 18:48:29 +0800

    modify by Git

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
+  cout<<"add this by Git - nano"<<endl;
+
   cout<<"add something here"<<endl;
   cout<<"add more thing here"<<endl;
   cout<<"Hello World!"<<endl;
